1f9b8be41af99c768d3d50f0be57e22c2ac67314
Rok Rejc <rok@rejc.org>
2023-01-23 11:16:55 +0100

    Include batch files for building Docker images.

---
+++ build-docker.bat
@@ -0,0 +1,2 @@
+docker build -t gos-web -f src/Gos.Web/Dockerfile .
+docker build -t gos-console -f src/Gos.Console/Dockerfile .
---
+++ build-publish-docker.bat
@@ -0,0 +1,17 @@
+docker build -t gos-web -f src/Gos.Web/Dockerfile .
+docker build -t gos-console -f src/Gos.Console/Dockerfile .
+
+docker login ghcr.io
+
+docker tag gos-web ghcr.io/clarinsi/gos-web:latest
+docker tag gos-web ghcr.io/clarinsi/gos-web:v1.0.0
+docker tag gos-web ghcr.io/clarinsi/gos-web:v1.0
+docker tag gos-web ghcr.io/clarinsi/gos-web:v1
+
+docker tag gos-console ghcr.io/clarinsi/gos-console:latest
+docker tag gos-console ghcr.io/clarinsi/gos-console:v1.0.0
+docker tag gos-console ghcr.io/clarinsi/gos-console:v1.0
+docker tag gos-console ghcr.io/clarinsi/gos-console:v1
+
+docker push --all-tags ghcr.io/clarinsi/gos-web
+docker push --all-tags ghcr.io/clarinsi/gos-console
